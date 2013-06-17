63c4526d97df950d862e0605769ca7c666613bbe
Ritchie <skawful@gmail.com>
2013-06-17 16:41:22 -0700

    Api updates, discover script

--- data-sources/oracle.js
+++ data-sources/oracle.js
@@ -4,14 +4,24 @@
 
 var asteroid = require('asteroid');
 
-// export the oracle data source
-module.exports = asteroid.createDataSource({
-  adapter: 'oracle',
-  host: '166.78.158.45',
-  database: 'XE',
-  username: 'strongloop',
-  password: 'str0ng100pjs',
-  debug: false
-});
-
-var Location = require('../models/location');
+if(process.env.NODE_ENV === 'test') {
+  console.log('-----TEST-----');
+  
+  // use memory adapter
+  module.exports = asteroid.createDataSource({
+    connector: require('asteroid').Memory
+  });
+  
+  // import data
+  require('../test-data/import');
+} else {
+  // export the oracle data source
+  module.exports = asteroid.createDataSource({
+    connector: require('jugglingdb-oracle'),
+    host: '166.78.158.45',
+    database: 'XE',
+    username: 'blackpool',
+    password: 'str0ng100pjs',
+    debug: false
+  });
+}
