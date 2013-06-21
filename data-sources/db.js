e80d72a4443da1ae7deea94cbd61be80efd99392
Raymond Feng <raymond@strongloop.com>
2013-06-20 17:51:07 -0700

    Rename oracle connector

--- data-sources/db.js
+++ data-sources/db.js
@@ -17,7 +17,7 @@ if(process.env.NODE_ENV === 'test') {
 } else {
   // export the oracle data source
   module.exports = asteroid.createDataSource({
-    connector: require('jugglingdb-oracle'),
+    connector: require('asteroid-connector-oracle'),
     host: '166.78.158.45',
     database: 'XE',
     username: 'blackpool',
