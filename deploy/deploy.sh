af950a34105a3830ebd9633657cd6ad4c0b7951f
Ryan Graham <ryan@strongloop.com>
2014-01-16 14:15:55 -0800

    deploy: Tell user about deployment assumptions

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -4,36 +4,61 @@ BASE=`pwd`
 APP_NAME=`basename $BASE`
 TARBALL="${APP_NAME}.tgz"
 
-function usage() {
+function do_usage() {
     echo "$0 --to=<server> [--as=<user>] [--port=<PORT>]"
+    echo "$0 --check --to=<server> [--as=<user>] [--port=<PORT>]"
 }
 
-function run() {
+function do_check() {
     to=$1; as=$2; port=$3
-    dest=$to
+
     if [[ -z "$as" ]]; then
         as=`id -un`
     fi
     if [[ -z "$port" ]]; then
         port=22
     fi
+
+    echo "Based on your provided arguments, this script assumes that:"
+    grep -e 'ASSUMPTION[:]' $0 | \
+        sed -e "s/APP_NAME/$APP_NAME/g" | \
+        sed -e "s/DEPLOYER/$as/g" | \
+        sed -e "s/.*# ASSUMPTION[:]/ */g"
+}
+
+function do_run() {
+    to=$1; as=$2; port=$3
+
+    if [[ -z "$as" ]]; then
+        as=`id -un`
+    fi
+    if [[ -z "$port" ]]; then
+        port=22
+    fi
+
     echo "Deploying $TARBALL => $as@$to:$port:/apps/$APP_NAME"
-    # ASSUMPTION: Apps are deployed to /apps/<NAME>
-    # ASSUMPTION: /apps/<NAME> is writable by deploying user
+    # ASSUMPTION: APP_NAME is deployed to /apps/APP_NAME
+    # ASSUMPTION: /apps/APP_NAME is writable by DEPLOYER
     cat $TARBALL | ssh -p $port $as@$to tar -C /apps/$APP_NAME --strip-components 1 -xzf -
-    # ASSUMPTION: /etc/init/<NAME>.conf is writable by deploying user
+
+    echo "Configuring Upstart job: $APP_NAME"
+    # ASSUMPTION: /etc/init/APP_NAME.conf is writable by deploying user
     cat deploy/upstart.conf | \
         sed -e "s/APP_NAME/$APP_NAME/g" | \
         sed -e "s/DEPLOYER/$as/g" | \
         ssh -p $port $as@$to "cat - > /etc/init/${APP_NAME}.conf"
-    # ASSUMPTION: deploying user has sudo permission for at least /sbin/initctl
+
+    echo "Reloading/Restarting Upstart job: $APP_NAME"
+    # ASSUMPTION: DEPLOYER has sudo permission to run /sbin/initctl
     ssh -p $port $as@$to "sudo /sbin/initctl reload $APP_NAME; \
                           sudo /sbin/initctl restart $APP_NAME || \
                           sudo /sbin/initctl start $APP_NAME"
 }
 
 if [[ -z "$npm_config_to" ]]; then
-    usage
+    do_usage
+elif [[ -n "$npm_config_check" ]]; then
+    do_check $npm_config_to $npm_config_as $npm_config_port
 else
-    run $npm_config_to $npm_config_as $npm_config_port
+    do_run $npm_config_to $npm_config_as $npm_config_port
 fi
