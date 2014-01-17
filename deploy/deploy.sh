b40f54c5192e32cb620656caccfd212ed2e1e775
Ryan Graham <ryan@strongloop.com>
2014-01-16 17:29:50 -0800

    deploy: Parameterize PORT and DB for deployments

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
 
-BASE=`pwd`
-APP_NAME=`basename $BASE`
-TARBALL="${APP_NAME}.tgz"
-
 function do_usage() {
-    echo "$0 --to=<host> [--user=<user>] [--ssh-port=<port>]"
-    echo "$0 --check --to=<host> [--user=<user>] [--ssh-port=<port>]"
+    echo "$0 --to=<host> [--deployer=<user>] [--ssh-port=<port>]"
+    echo "$0 --check --to=<host> [--deployer=<user>] [--ssh-port=<port>]"
 }
 
 function do_check() {
@@ -29,8 +25,8 @@ function do_check() {
 function do_run() {
     to=$1; user=$2; port=$3
 
-    if [[ ! -f "${APP_NAME}.tgz" ]]; then
-        echo "${APP_NAME}.tgz not found, please run 'npm run bundle-pack'"
+    if [[ ! -f "${TARBALL}" ]]; then
+        echo "${TARBALL} not found, please run 'npm run bundle-pack'"
         exit
     fi
 
@@ -44,13 +40,17 @@ function do_run() {
     echo "Deploying $TARBALL => $user@$to:$port:/apps/$APP_NAME"
     # ASSUMPTION: APP_NAME is deployed to /apps/APP_NAME
     # ASSUMPTION: /apps/APP_NAME is writable by DEPLOYER
-    cat $TARBALL | ssh -p $port $user@$to tar -C /apps/$APP_NAME --strip-components 1 -xzf -
+    cat $TARBALL | ssh -p $port $user@$to \
+        "mkdir -p /apps/${APP_NAME}; \
+        tar -C /apps/$APP_NAME --strip-components 1 -xzf -"
 
     echo "Configuring Upstart job: $APP_NAME"
     # ASSUMPTION: /etc/init/APP_NAME.conf is writable by deploying user
     cat deploy/upstart.conf | \
         sed -e "s/APP_NAME/$APP_NAME/g" | \
         sed -e "s/DEPLOYER/$user/g" | \
+        sed -e "s/DEPLOY_DB/$npm_config_db/g" | \
+        sed -e "s/DEPLOY_PORT/$npm_config_port/g" | \
         ssh -p $port $user@$to "cat - > /etc/init/${APP_NAME}.conf"
 
     echo "Reloading/Restarting/Starting Upstart job: $APP_NAME"
@@ -60,10 +60,17 @@ function do_run() {
                           sudo /sbin/initctl start $APP_NAME"
 }
 
+: ${npm_package_name:=$(basename $(pwd))}
+: ${npm_config_db:=""}
+: ${npm_config_port:=${PORT:-3000}}
+TARBALL=${1:-${npm_package_name}.tgz}
+APP_NAME=${npm_config_as:-${npm_package_name}}
+
+
 if [[ -z "$npm_config_to" ]]; then
     do_usage
 elif [[ -n "$npm_config_check" ]]; then
-    do_check $npm_config_to $npm_config_as $npm_config_ssh_port
+    do_check $npm_config_to $npm_config_deployer $npm_config_ssh_port
 else
-    do_run $npm_config_to $npm_config_as $npm_config_ssh_port
+    do_run $npm_config_to $npm_config_deployer $npm_config_ssh_port
 fi
