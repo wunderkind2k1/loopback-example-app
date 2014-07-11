4b93168574d2e0d6a18f4bd1d102a64cdc7c6404
Miroslav Bajtoš <miroslav@strongloop.com>
2014-07-11 09:41:52 +0200

    Upgrade to the new project layout
    Re-create the sls-sample-app using `yo loopback:example`.

--- models/location.js
+++ models/location.js
@@ -1,94 +1,62 @@
-/**
- * Module Dependencies
- */
+module.exports = function(RentalLocation, Base) {
+  RentalLocation.nearby = function(here, page, max, fn) {
+    if (typeof page === 'function') {
+      fn = page;
+      page = 0;
+      max = 0;
+    }
 
-var db = require('../data-sources/db');
-var config = require('./location.json');
-var loopback = require('loopback');
-var GeoPoint = loopback.GeoPoint;
-var TaskEmitter = require('strong-task-emitter');
-var rest = require('../data-sources/rest-geocode');
-var Inventory = require('./inventory');
+    if (typeof max === 'function') {
+      fn = max;
+      max = 0;
+    }
 
-/**
- * location Model
- */
+    var limit = 10;
+    page = page || 0;
+    max = Number(max || 100000);
 
-var RentalLocation = module.exports = db.createModel(
-  'location',
-  config.properties,
-  config.options
-);
+    RentalLocation.find({
+      // find locations near the provided GeoPoint
+      where: {geo: {near: here, maxDistance: max}},
+      // paging
+      skip: limit * page,
+      limit: limit
+    }, fn);
+  };
 
-/**
- * Each location has inventory.
- */
+  RentalLocation.beforeSave = function(next, loc) {
+    if (loc.geo) return next();
 
-RentalLocation.hasMany(Inventory);
+    // geo code the address
+    var geoService = RentalLocation.app.dataSources.geo;
+    geoService.geocode(loc.street, loc.city, loc.state,
+      function(err, result) {
+        if (result && result[0]) {
+          loc.geo = result[0].lng + ',' + result[0].lat;
+          next();
+        } else {
+          next(new Error('could not find location'));
+        }
+      });
+  };
 
-/**
- * Find nearby locations.
- */
+  RentalLocation.setup = function() {
+    Base.setup.apply(this, arguments);
 
-RentalLocation.nearby = function(here, page, max, fn) {
-  if (typeof page === 'function') {
-    fn = page;
-    page = 0;
-    max = 0;
-  }
-
-  if (typeof max === 'function') {
-    fn = max;
-    max = 0;
-  }
-
-  var limit = 10;
-  page = page || 0;
-  max = Number(max || 100000);
-
-  RentalLocation.find({
-    // find locations near the provided GeoPoint
-    where: {geo: {near: here, maxDistance: max}},
-    // paging
-    skip: limit * page,
-    limit: limit
-  }, fn);
-};
-
-/**
- * Expose nearby as a remote method.
- */
-
-loopback.remoteMethod(
-  RentalLocation.nearby,
-  {
-    description: 'Find nearby locations around the geo point',
-    accepts: [
-      {arg: 'here', type: 'GeoPoint', required: true, description: 'geo location (lat & lng)'},
-      {arg: 'page', type: 'Number', description: 'number of pages (page size=10)'},
-      {arg: 'max', type: 'Number', description: 'max distance in miles'}
-    ],
-    returns: {arg: 'locations', root: true}
-  }
-);
-
-/**
- * Build the geo data when saving using the google maps api.
- */
-
-RentalLocation.beforeSave = function(next, loc) {
-  // geo code the address
-  if (!loc.geo) {
-    rest.geocode(loc.street, loc.city, loc.state, function(err, result, res) {
-      if (result && result[0]) {
-        loc.geo = result[0].lng + ',' + result[0].lat;
-        next();
-      } else {
-        next(new Error('could not find location'));
-      }
+    this.remoteMethod('nearby', {
+      description: 'Find nearby locations around the geo point',
+      accepts: [
+        {arg: 'here', type: 'GeoPoint', required: true,
+          description: 'geo location (lat & lng)'},
+        {arg: 'page', type: 'Number',
+          description: 'number of pages (page size=10)'},
+        {arg: 'max', type: 'Number',
+          description: 'max distance in miles'}
+      ],
+      returns: {arg: 'locations', root: true},
+      http: { verb: 'GET' }
     });
-  } else {
-    next();
-  }
-};
+  };
 
+  RentalLocation.setup();
+};
---
+++ rest/boot/authentication.js
@@ -0,0 +1,4 @@
+module.exports = function enableAuthentication(rest) {
+  // enable authentication
+  rest.enableAuth();
+};
